90185aa1538648fbe0f4913e1c3dd04ba4a174cf
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-11 15:39:20 +0100

    Add validation and migrate

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_many :transactions, through: :category_transtactions, dependent: :destroy
   has_many :category_transtactions, dependent: :destroy
+  validates :name, presence: true
+  validates :icon, presence: true
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,6 @@
 class Transaction < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_one :category, through: :category_transactions
-  has_one :category_transaction
+  has_many :categorys, through: :category_transactions
+  has_many :category_transactions
+  validates :name, :amount, presence: true
 end
