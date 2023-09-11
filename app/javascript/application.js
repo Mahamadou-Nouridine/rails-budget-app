7930959fdcf15094ddb521780f26654daf9e367e
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-11 13:30:56 +0100

    Add categories controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+/* app/assets/javascripts/application.js */
+
+//= require popper
+//= require bootstrap
 import "@hotwired/turbo-rails"
 import "controllers"
