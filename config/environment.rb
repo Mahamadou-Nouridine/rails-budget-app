064889d827b8a8fa5dd50d0d976cfb32f7d728bf
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-13 20:16:55 +0100

    Setup deployment

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # Load the Rails application.
 require_relative "application"
 
+config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || ENV['RENDER'].present?
 # Initialize the Rails application.
 Rails.application.initialize!
