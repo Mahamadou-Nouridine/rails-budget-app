c55a26e3037a952c46b0eeaf112c7669c2e2be47
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-13 11:46:39 +0100

    Stylize auth page

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,5 @@
+class AuthController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/auth', to: 'auth#index'
   root "categorys#index"
   resources :categorys, only: [:show, :index, :new, :create] do
     resources :transactions, only: [:new, :create]
