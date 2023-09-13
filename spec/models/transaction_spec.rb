d94177d7cd9700665bed88541934233db863ca3c
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-13 23:19:42 +0100

    Add model testing to transaction model

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,22 +1,22 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe Transaction, type: :model do
-#   let (:user) {User.create(name: 'Nouridine', email: 'nouridine@gmail.com', password: 'abcdef')}
-#   let(:transaction) { Transaction.new(name: 'Macdo', author: user, amount: 20) }
+RSpec.describe Transaction, type: :model do
+  let (:user) {User.create(name: 'Nouridine', email: 'nouridine@gmail.com', password: 'abcdef')}
+  let(:transaction) { Transaction.new(name: 'Macdo', author: user, amount: 20) }
 
-#   it 'the transaction should be valid' do
-#     expect(transaction).to be_valid
-#   end
-#   it 'Without the name, the transaction is not valid' do
-#     transaction.name = ''
-#     expect(transaction).not_to be_valid
-#   end
-#   it 'Without the author, the transaction is not valid' do
-#     transaction.author = nil
-#     expect(transaction).not_to be_valid
-#   end
-#   it 'Without the amount, the transaction is not valid' do
-#     transaction.amount = nil
-#     expect(transaction).not_to be_valid
-#   end
-# end
+  it 'the transaction should be valid' do
+    expect(transaction).to be_valid
+  end
+  it 'Without the name, the transaction is not valid' do
+    transaction.name = ''
+    expect(transaction).not_to be_valid
+  end
+  it 'Without the author, the transaction is not valid' do
+    transaction.author = nil
+    expect(transaction).not_to be_valid
+  end
+  it 'Without the amount, the transaction is not valid' do
+    transaction.amount = nil
+    expect(transaction).not_to be_valid
+  end
+end
