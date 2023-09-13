ddd02c282f827035d4748e2ddee96df60e2fdd81
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-13 15:30:14 +0100

    Fix broken js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,31 @@
 class ApplicationController < ActionController::Base
-  before_action :set_variables
+  # before_action :set_variables
+  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
-  def set_variables
-    @current_user = current_user
+  def after_sign_out_path_for(resource_or_scope)
+    auth_path
   end
 
-  def current_user
-    User.first
+  def authenticate_user!
+    if !user_signed_in?
+      current_uri = request.env['PATH_INFO']
+      if !([auth_path, "/users/sign_in", "/users/sign_up", "/users/password/new", user_password_path].include?(current_uri))
+        redirect_to auth_path
+      end
+    end
   end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:email, :password, :password_confirmation, :name)
+    end
+  end
+
+  # def set_variables
+  #   @current_user = current_user
+  # end
 end
--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,7 @@
 class AuthController < ApplicationController
   def index
-
+    if user_signed_in?
+      redirect_to root_path
+    end
   end
 end
