131d13c88f6c2f3f24a45a548082ffe125d0f252
mahamadou-nouridine <mahamadounouridinem@gmail.com>
2023-09-14 13:18:59 +0100

    Add categories selection when creating a transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -10,6 +10,11 @@ class TransactionsController < ApplicationController
     @category_transaction = CategoryTransaction.new(expense: @transaction, category_id: params[:category_id])
 
     if @transaction.save && @category_transaction.save
+      if params[:categorys]
+        params[:categorys].each do |category_id|
+          CategoryTransaction.create(expense: @transaction, category_id: category_id)
+        end
+      end
       redirect_to category_path(params[:category_id]), notice: 'Transaction created successfully'
     else
       redirect_to category_path(params[:category_id]), alert: 'An error occured when creating the transaction'
